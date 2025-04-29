ea0c0bce9751086e174ee2a97e7c217c16256a4f
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-29 16:52:41 -0400

    Reposicionando elementos en filas y columnas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,12 @@
     {{-- A diferencia de bootstrap en tailwind no es obligatorio un container para usar el grid --}}
     <section class="container">
         {{-- La grid de tailwind igual que la de bootstrap es de un máximo de 12 columnas --}}
-        <div class="grid grid-cols-4 gap-4">
-            <article class="bg-blue-100 text-gray-950 text-center py-3 rounded-xl">Artículo 1</article>
-            <article class="bg-blue-200 text-gray-950 text-center py-3 rounded-xl col-start-4">Artículo 2</article>
-            <article class="bg-blue-300 text-gray-950 text-center py-3 rounded-xl col-span-2 col-start-2">Artículo 3</article>
-            <article class="bg-blue-400 text-gray-950 text-center py-3 rounded-xl col-start-1">Artículo 4</article>
-            <article class="bg-blue-500 text-white text-center py-3 rounded-xl col-end-5">Artículo 5</article>
+        <div class="grid grid-cols-4 grid-rows-2 gap-4">
+            <article class="bg-blue-100 text-gray-950 text-center py-3 rounded-xl col-span-2 col-start-2 row-span-2">Artículo 1</article>
+            <article class="bg-blue-200 text-gray-950 text-center py-3 rounded-xl col-start-1 row-end-1">Artículo 2</article>
+            <article class="bg-blue-300 text-gray-950 text-center py-3 rounded-xl">Artículo 3</article>
+            <article class="bg-blue-400 text-gray-950 text-center py-3 rounded-xl col-end-5">Artículo 4</article>
+            <article class="bg-blue-500 text-white text-center py-3 rounded-xl col-start-1 row-end-2">Artículo 5</article>
         </div>
     </section>
 </body>
