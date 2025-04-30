b900877bff88fa01ca4c6e812a719f6f5443cf8c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-29 21:18:34 -0400

    Combinando el aspect-video y el object-cover y object-center se pueden obtener resultados muy interesantes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,7 +15,12 @@
                 {{-- Se copió el iframe de un video de youtube, esto se logra con compartir/insertar --}}
 
                 {{-- Se eliminaron el alto y ancho fijos y se le dió un ancho 100% y aspect-video para que se vea bien --}}
-                <iframe class="w-full aspect-video" src="https://www.youtube.com/embed/hQps7n-o5n8?si=_7HWKsg4GixOAipL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                {{-- object-cover se aplica para que el iframe cubra todo el contenedor sin deformarse --}}
+                <iframe class="w-full aspect-video object-cover object-center" src="https://www.youtube.com/embed/hQps7n-o5n8?si=_7HWKsg4GixOAipL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+                <div class="bg-red-400 aspect-2/1">
+
+                </div>
             </div>
 
             <div class="col-span-1 bg-blue-200">
