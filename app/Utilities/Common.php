b7b0620098da4155c4f9fe4c605b045c694586c9
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-30 16:16:58 -0400

    Lookup table inline, tiene una sintaxis más agradable para mi

--- app/Utilities/Common.php
+++ app/Utilities/Common.php
@@ -6,8 +6,8 @@ class Common
 {
     public static function validateMontserratType(string $montserrat): string
     {
-        // Lookup table o mapeo de búsqueda, esto sustituye al switch
-        $map = [
+        // Lookup table o mapeo de búsqueda, esto sustituye al switch - (Sintaxis básica)
+        /*$map = [
             'font-thin'      => 'Montserrat fina',
             'font-regular'   => 'Montserrat regular',
             'font-bold'      => 'Montserrat negrita',
@@ -15,6 +15,17 @@ class Common
             'font-black'     => 'Montserrat súper negrita',
         ];
 
-        return $map[$montserrat] ?? 'La fuente no es Montserrat';
+        return $map[$montserrat] ?? 'La fuente no es Montserrat';*/
+
+        // Lookup sintaxis inline
+        $lookup = [
+            'font-thin'      => 'Montserrat fina',
+            'font-regular'   => 'Montserrat regular',
+            'font-bold'      => 'Montserrat negrita',
+            'font-extrabold' => 'Montserrat muy negrita',
+            'font-black'     => 'Montserrat súper negrita',
+        ][$montserrat] ?? 'La fuente no es Montserrat';
+
+        return $lookup;
     }
 }
