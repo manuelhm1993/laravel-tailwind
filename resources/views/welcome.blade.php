30bc34f3d27833b137386e08cad25007fc74a72d
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-30 12:24:11 -0400

    Instalación de una fuente externa, en este caso Montserrat, se creó resources/assets/fonts/Montserrat y ahí se colocó la instalación de la fuente, luego se creó el archivo resources/css/fonts.css para definir la familia Montserrat en el proyecto y finalmente se actualizó el tema creando una clase o variable personalizada para tailwind luego de importar fonts, esto permite usar la fuente personalizada en todo el proyecto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     {{-- Se recomienda sustituir el space-y-4 en elementos grandes por flex flex-col gap-4 en la v4 --}}
     <section class="container py-12 space-y-4">
         {{-- Tailwind no impone un diseño pre-definido, pero proporciona clases para facilitar el mismo, las tres fuentes por defecto que maneja tailwind son font-sans, font-serif, font-mono --}}
-        <h1 class="h1">Este es un título de prueba</h1>
+        <h1 class="h1 font-montserrat font-bold">Este es un título de prueba</h1>
         {{-- El interlineado por defecto es leading-6, a partir de ahí se puede disminuir o aumentar --}}
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque aliquid provident iste magni eaque autem ducimus repudiandae accusamus, voluptatum tempora, eos deleniti soluta porro commodi neque architecto, dolores laborum nostrum.
