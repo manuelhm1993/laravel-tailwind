e28a9dd85e1814c3e35dc3bd5ed4bb51d9c10093
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-30 16:38:15 -0400

    Para trabajar con listas en lugar de agregar px se debe usar list-inside para ubicarlas correctamente dentro del contenedor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,10 @@
     <section class="container py-12 flex flex-col gap-4">
         <h1 class="h1 font-montserrat font-bold">Listas ordenadas</h1>
 
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odio consequuntur minus, harum aliquid amet distinctio laborum accusantium doloribus cupiditate autem soluta magni eos sed ipsam quidem suscipit ea dolore?</p>
+
         {{-- Manipulación de los estilos de la lista --}}
-        <ol class="list-decimal px-5">
+        <ol class="list-decimal list-inside">
             @foreach ($content as $parrafo)
                 <li>{{ $parrafo }}</li>
             @endforeach
