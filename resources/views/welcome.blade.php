1ba86b9943d613dd38d0372c051bac3cbb319b56
Manuel Henriquez <manuelhm1993@gmail.com>
2025-04-30 16:29:44 -0400

    Para darle los estilos de lista desordenada se usa la clase list-disc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,17 +11,14 @@
 <body>
     {{-- Se recomienda sustituir el space-y-4 en elementos grandes por flex flex-col gap-4 en la v4 --}}
     <section class="container py-12 flex flex-col gap-4">
-        @forelse ($content as $classes => $parrafo)
-            <h2 class="font-montserrat font-bold text-2xl">
-                {{-- Enviar la segunda clase para validarla --}}
-                {{ App\Utilities\Common::validateMontserratType(explode(' ', $classes)[1] ?? '') }}
-            </h2>
+        <h1 class="h1 font-montserrat font-bold">Listas desordenadas</h1>
 
-            {{-- Manipular el espaciado entre letras: letter-spacing --}}
-            <p class="{{ (explode(' ', $classes)[1] == 'font-regular') ? $classes . ' tracking-widest' : $classes }}">{{ $parrafo }}</p>
-        @empty
-            <h1 class="h1 font-bold">Sin contenido</h1>
-        @endforelse
+        {{-- Manipulación de los estilos de la lista --}}
+        <ul class="list-disc px-5">
+            @foreach ($content as $parrafo)
+                <li>{{ $parrafo }}</li>
+            @endforeach
+        </ul>
     </section>
 </body>
 </html>
