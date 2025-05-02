d7640808cb3e3a0e214f0432c6f2a67850d0cdc4
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-02 14:45:01 -0400

    Se aprendió a usar imágenes como fondo de un contenedor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,22 +7,27 @@
     <title>Laravel - Tailwindcss</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <style>
+        /* Clase personalizada para obtener una img por background */
+        .imagen {
+            background-image: url("{{ asset('storage/img/pexels-thngocbich-1714341.jpg') }}");
+            height: 400px;
+            width: 400px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
-    {{-- Se recomienda sustituir el space-y-4 en elementos grandes por flex flex-col gap-4 en la v4 --}}
-    <section class="container py-12 flex flex-col gap-4">
-        {{-- La clase text-x permite alinear el texto de un contenedor --}}
-        <h1 class="h1 font-montserrat font-bold text-left md:text-center lg:text-right uppercase">Listas ordenadas</h1>
+    <section class="container">
+        <h1 class="h1 font-bold text-center my-3">Backgrounds</h1>
 
-        {{-- Manipulación de colores --}}
-        <p class="text-red-700 hover:text-blue-700 hover:no-underline line-through">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil odio consequuntur minus, harum aliquid amet distinctio laborum accusantium doloribus cupiditate autem soluta magni eos sed ipsam quidem suscipit ea dolore?</p>
+        <p>{{ $content[key($content)] }}</p>
 
-        {{-- Manipulación de los estilos de la lista --}}
-        <ol class="list-decimal md:list-disc list-inside">
-            @foreach ($content as $parrafo)
-                <li>{{ $parrafo }}</li>
-            @endforeach
-        </ol>
+        {{-- Obligar a la imagen a adaptarse a las dimensiones del contenedor --}}
+        <figure class="imagen bg-contain">
+
+        </figure>
     </section>
 </body>
 </html>
