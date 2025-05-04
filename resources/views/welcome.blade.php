6861b978b07a825dc0eeb8d2e8fb9d9b7df1890f
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-04 15:06:57 -0400

    Forma de manipular el axis con justify-*, ubica los elementos según la clase indicada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
             <h1>Layout</h1>
 
             {{-- Contenedor flex, todas las cajas deben ir dentro de un contenedor --}}
-            <div class="bg-gray-300 flex flex-col">
+            <div class="bg-gray-300 flex justify-end">
                 {{-- 
                     Ejes flex
                         * Eje main-axis: por defecto es flex-row, pero existe flex-col
