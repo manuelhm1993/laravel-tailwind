5e11def54933f7192463da98063953b5ce4e29d3
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-03 21:48:23 -0400

    El w-full no es necesario si se está dentro de flex, se puede jugar con los bordes collapse y separate por breakpoins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,30 +11,33 @@
 <body>
     {{-- Contenedor principal --}}
     <section class="container py-5">
-        {{-- A diferencia de bootstrap, tailwind no impone un diseño predefinido, se debe hacer el diseño de la tabla y luego crear la clase table con @apply --}}
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>País</th>
-                    <th>Ciudad</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr>
-                    <td>Venezuela</td>
-                    <td>Maracaibo</td>
-                </tr>
-                <tr>
-                    <td>Colombia</td>
-                    <td>Bogotá</td>
-                </tr>
-                <tr>
-                    <td>España</td>
-                    <td>Madrid</td>
-                </tr>
-            </tbody>
-        </table>
+        {{-- Alinear verticalmente el contenido del div y darle separación de 16px = 1rem --}}
+        <div class="flex flex-col gap-4">
+            <h1>Tablas</h1>
+            {{-- A diferencia de bootstrap, tailwind no impone un diseño predefinido, se debe hacer el diseño de la tabla y luego crear la clase table con @apply --}}
+            <table class="table border-separate md:border-collapse">
+                <thead>
+                    <tr>
+                        <th>País</th>
+                        <th>Ciudad</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Venezuela</td>
+                        <td>Maracaibo</td>
+                    </tr>
+                    <tr>
+                        <td>Colombia</td>
+                        <td>Bogotá</td>
+                    </tr>
+                    <tr>
+                        <td>España</td>
+                        <td>Madrid</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </section>
 </body>
 </html>
