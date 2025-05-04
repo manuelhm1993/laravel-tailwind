15757398d2ebf54ad9ceab6e02755232336c1f7c
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-04 15:58:39 -0400

    cross-axis por defecto trabaja con items-strech, esto hace que por defecto main-axis muestre los items en una única línea y si su ancho no se ajusta a la pantalla, reduce el tamaño, esto puede ser modificado para el responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,19 +16,21 @@
             <h1>Layout</h1>
 
             {{-- Contenedor flex, todas las cajas deben ir dentro de un contenedor --}}
-            <div class="bg-gray-300 flex h-64 justify-center items-center">
+            <div class="bg-gray-300 flex h-64 justify-center">
                 {{-- 
                     Ejes flex
                         * Eje main-axis: por defecto es flex-row, pero existe flex-col
                             - Row: distribuye las cajas de izquierda a derecha en filas
                             - Col: distribuye las cajas de arriba a abajo en columnas
-                        * Eje cross-axis: es un eje perpendicular al main-axis
+                            - Clases: justify
+                        * Eje cross-axis: por defecto es items-strech, es un eje perpendicular al main-axis
                             - Si el eje main-axis es horizontal, el eje cross-axis será vertical
                             - El eje cross-axis establece que se ocupe todo el espacio posible
+                            - Clases: items
                 --}}
-                <div class="bg-gray-400 text-gray-700 py-2 px-4 m-2">1</div>
-                <div class="bg-gray-400 text-gray-700 py-2 px-4 m-2">2</div>
-                <div class="bg-gray-400 text-gray-700 py-2 px-4 m-2">3</div>
+                <div class="bg-gray-400 text-gray-700 py-2 px-4 m-2 w-64">1</div>
+                <div class="bg-gray-400 text-gray-700 py-2 px-4 m-2 w-64">2</div>
+                <div class="bg-gray-400 text-gray-700 py-2 px-4 m-2 w-64">3</div>
             </div>
         </div>
     </section>
