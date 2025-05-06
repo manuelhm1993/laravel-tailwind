5e117ef9335e2180466bc605fd6af8e4a2aae0c5
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-06 01:33:05 -0400

    Como livewire 3 ya integra Apline, simplemente se puede utilizar sin ningún tipo de restricción

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -35,7 +35,8 @@
                     <!-- Profile dropdown --> <!-- Alpine trata a los elementos reactivos como componentes x-data -->
                     <div class="relative ml-3" x-data="{ open: false }">
                         <div>
-                            <button type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                            {{-- Las directivas de Alpine son muy similares a Vue --}}
+                            <button x-on:click="open = ! open" type="button" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">Open user menu</span>
                                 <img class="size-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
