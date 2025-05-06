8181526dca79054b693a1786c3be26580bf9bef2
Manuel Henriquez <manuelhm1993@gmail.com>
2025-05-06 02:23:57 -0400

    Se aprendió el concepto de las islas de componentes, en ese sentido para que se puedan afectar desde fuera de un ámbito, las variables x-data deben afectar a los distintos componentes, esto es estando en un contenedor común, en este caso nav

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gray-800">
+<nav class="bg-gray-800" x-data="{ menuMovil: false }">
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div class="flex h-16 items-center justify-between">
             <div class="flex items-center">
@@ -56,7 +56,7 @@
             
             {{-- Menú de hamburguesa --}}
             <div class="-mr-2 flex md:hidden">
-                <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" aria-controls="mobile-menu" aria-expanded="false">
+                <button x-on:click="menuMovil = !menuMovil" type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" aria-controls="mobile-menu" aria-expanded="false">
                     <span class="absolute -inset-0.5"></span>
                     <span class="sr-only">Open main menu</span>
                     <!-- Menu open: "hidden", Menu closed: "block" -->
@@ -78,7 +78,7 @@
     <!-- Menú móvil -->
     <div class="md:hidden" id="mobile-menu">
         {{-- Menú --}}
-        <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+        <div x-show="menuMovil" class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
             <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
@@ -88,7 +88,7 @@
         </div>
 
         {{-- Sección de perfil y notificaciones --}}
-        <div class="border-t border-gray-700 pt-4 pb-3">
+        <div x-show="menuMovil" class="border-t border-gray-700 pt-4 pb-3">
             <div class="flex items-center px-5">
                 <div class="shrink-0">
                     <img class="size-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
